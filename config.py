c6c0a43f2ec2d2817e3005a3f318dec5e729a5f6
Luis Daniel <luisdaniel.14777@gmail.com>
2026-07-13 23:10:04 -0500

    Confirmar diagnostico

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ DB_USER = os.getenv("DB_USER") or os.getenv("MYSQLUSER") or "root"
 
 DB_PASSWORD = os.getenv("DB_PASSWORD") or os.getenv("MYSQLPASSWORD") or ""
 
-DB_NAME = os.getenv("DB_NAME") or os.getenv("MYSQLDATABASE") or "inventario_pinturas"
+DB_NAME = os.getenv("DB_NAME") or os.getenv("MYSQLDATABASE") or "railway"
